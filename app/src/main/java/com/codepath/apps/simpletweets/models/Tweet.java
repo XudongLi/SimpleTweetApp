8358f19c38a4e996c79fa2d6c0ae0e267796c25b
XudongLi <xudongli@uw.edu>
2016-10-29 12:37:39 -0700

    add the app framework with basic function like calling TwitterApi and setup OAuth

---
+++ app/src/main/java/com/codepath/apps/simpletweets/models/Tweet.java
@@ -0,0 +1,62 @@
+package com.codepath.apps.simpletweets.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class Tweet {
+
+    // List out the attribute
+    private String body;
+    private long uid;
+    private String createdAt;
+    private User user;
+
+    public static Tweet fromJSON(JSONObject jsonObject) {
+        Tweet tweet = new Tweet();
+        try {
+            tweet.body = jsonObject.getString("text");
+            tweet.uid = jsonObject.getLong("id");
+            tweet.createdAt = jsonObject.getString("created_at");
+            tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return tweet;
+    }
+
+    public static ArrayList<Tweet> fromJSONArray(JSONArray jsonArray) {
+        ArrayList<Tweet> tweets = new ArrayList<>();
+        for(int i = 0; i < jsonArray.length(); i++) {
+            try {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                Tweet tweet = fromJSON(jsonObject);
+                if (tweet != null) {
+                    tweets.add(tweet);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                continue;
+            }
+        }
+        return tweets;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public long getUid() {
+        return uid;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
