7afb272a682f5b1baf42dcedfd5f9b9d1e9cb75e
XudongLi <xudongli@uw.edu>
2016-11-07 20:56:41 -0800

    minor UI change

--- app/src/main/java/com/codepath/apps/simpletweets/activities/ProfileActivity.java
+++ app/src/main/java/com/codepath/apps/simpletweets/activities/ProfileActivity.java
@@ -1,9 +1,11 @@
 package com.codepath.apps.simpletweets.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.codepath.apps.simpletweets.R;
 import com.codepath.apps.simpletweets.fragments.ProfileHeaderFragment;
@@ -23,6 +25,15 @@ public class ProfileActivity extends AppCompatActivity {
     private void initializeViews(Bundle savedInstanceState) {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(android.support.design.R.drawable.abc_ic_ab_back_material);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent result = new Intent();
+                setResult(RESULT_CANCELED, result);
+                finish();
+            }
+        });
 
         String source = getIntent().getStringExtra("source");
         String screenName = getIntent().getStringExtra("screenName");
